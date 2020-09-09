88b2eb6f6215d820a3299373a0835a5b85abd73a
Le Thanh <home@Les-iMac.local>
2020-09-09 17:26:57 +0700

    add check out components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CheckOut from './components/checkout/CheckOut';
 
 function App() {
 	return (
-		<div className='app'>
-			<Header />
-			<Home />
-		</div>
+		<Router>
+			<div className='app'>
+				<Header />
+				<Switch>
+					<Route path='/checkout'>
+						<CheckOut />
+					</Route>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
---
+++ src/components/checkout/subtotal/Subtotal.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Subtotal.css';
+
+const Subtotal = () => {
+	return <div className='subtotal'>i am subtotal</div>;
+};
+
+export default Subtotal;
