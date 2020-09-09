bfe4a4fd8636735b8380e1a55bc25b7309c14aaa
Le Thanh <home@Les-iMac.local>
2020-09-09 20:23:48 +0700

    add state provider

--- src/components/checkout/subtotal/Subtotal.js
+++ src/components/checkout/subtotal/Subtotal.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './Subtotal.css';
+import CurrencyFormat from 'react-currency-format';
 
 const Subtotal = () => {
-	return <div className='subtotal'>i am subtotal</div>;
+	const ShoppingBasket = [];
+	const getBasketTotal = () => {};
+	const basket = [];
+	return (
+		<div className='subtotal'>
+			<CurrencyFormat
+				renderText={(value) => (
+					<>
+						<p>
+							Subtotal ({ShoppingBasket.length} items) :
+							<strong>{` ${value}`}</strong>
+						</p>
+						<small className='subtotal__gift'>
+							<input type='checkbox' /> This order contains a gift
+						</small>
+					</>
+				)}
+				decimalScale={2}
+				value={getBasketTotal(basket)}
+				displayType={'text'}
+				thousandSeparator={true}
+				prefix={'$'}
+			/>
+			<button>Procceed to Checkout</button>
+		</div>
+	);
 };
 
 export default Subtotal;
